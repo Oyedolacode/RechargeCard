bcafddfd4bb684c09bcf1f8bab37c4717cb73f10
Oyedolacode <segunzyshegszy12@gmail.com>
2021-02-20 07:05:09 -0800

    ADd f

---
+++ Recharge.php
@@ -0,0 +1,20 @@
+<?php
+
+//Header
+echo '<h1>Hurray!! Time to top Up</h1>';
+
+//Use a for loop to iterate our code
+for ($index=0;$index<200;$index++){
+    //Applying the Random function
+    
+    $TopUp = rand(10000000000000,90000000000000);
+    echo $index +1 . '.  '; 
+    echo 'PIN: ' . $TopUp."<br>";
+    echo 'SERIAL NO: '. rand(111000000000000,222222222222222).'<br>';
+    echo "<hr>";
+}
+
+
+
+
+?>
